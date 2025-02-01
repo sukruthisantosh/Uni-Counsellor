2d2c62ea00282c4d767981c9bdeba5128a982741
Sukruthi <sukruthisantosh@gmail.com>
2025-02-01 22:40:23 +0000

    feat: adds index.hmtl to public directory

--- Uni-Counsellor/uni-counsel/src/index.js
+++ Uni-Counsellor/uni-counsel/src/index.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-// import { ChakraProvider, CSSReset } from '@chakra-ui/react';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-      <App />
+    <App />
   </React.StrictMode>
 );
